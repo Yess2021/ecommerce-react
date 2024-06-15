2a33e7310d75212c33af6499337cdfdfd07d22c7
Yess <yessicarint@gmail.com>
2024-06-15 16:26:03 -0300

    Add pages

--- src/App.js
+++ src/App.js
@@ -4,21 +4,25 @@ import Layout from './components/Layout/Layout'
 import Inicio from './components/Inicio/Inicio'
 import Alta from './components/Alta/Alta'
 import Contacto from './components/Contacto/Contacto'
+import Nosotros from './components/Nosotros/Nosotros'
+import Favoritos from './components/Favoritos/Favoritos'
+import Registro from './components/Registro/Registro'
+import Carrito from './components/Carrito/Carrito'
+import NoMatch from './components/NoMatch/NoMatch'
 
 function App() {
   return (
     <div className="App">
-      Hola
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Inicio />} />
           <Route path="alta" element={<Alta />} />
           <Route path="contacto" element={<Contacto />} />
-          {/* <Route path="nosotros" element={<Nosotros />} />
+          <Route path="nosotros" element={<Nosotros />} />
           <Route path="favoritos" element={<Favoritos />} />
           <Route path="registro" element={<Registro />} />
           <Route path="carrito" element={<Carrito />} />
-          <Route path="*" element={<NoMatch />} /> */}
+          <Route path="*" element={<NoMatch />} /> 
         </Route>
       </Routes>
     </div>
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,29 +1,76 @@
 import { Outlet, Link } from "react-router-dom";
+import './Layout.css'
 
 const Layout = () => {
     return (
-      <div>
+      <body>
+       <header>
+        <div className="container-mariposa">
+            <h2 id="nombre-empresa">Mariposa 88</h2>
+            <div className="logo-mariposa"></div>
+        </div>
+
         <nav>
-          <ul>
-            <li>
-              <Link to="/">Inicio</Link>
-            </li>
-            <li>
-              <Link to="/alta">Alta</Link>
-            </li>
-            <li>
-              <Link to="/contacto">Contacto</Link>
-            </li>
-          </ul>
+            <ul>
+                <li> <Link to="/">Inicio</Link> </li>
+                <li> <Link to="/alta">Alta</Link> </li>
+                <li> <Link to="/contacto">Contacto</Link> </li>
+                <li> <Link to="/nosotros">Nosotros</Link> </li>
+            </ul>
         </nav>
-  
-        <hr />
+
+        <div className="iconos-header">
+            <a className="logoFav" href="" id="favoritos"></a>
+            <a className="logoRegistro" href="" id="registro"></a>
+
+            <div className="search-container">
+                <div id="logoBuscar"></div> 
+                <div id="barra-busqueda">
+                    <input type="text" id="input-busqueda" />
+                    <input type="button" value="Buscar" id="buscar" />
+                </div>
+            </div>
+
+            <a className="logoCarrito" href="" id="carrito">
+                <div id="contadorCarrito"></div>
+            </a>
+        </div>
+
+        <button className="menu-hamburger" id="hamburger"></button>
+
+    </header>
+
+    <div id="overlay">
+        <div className="mobile-menu">
+            <div className="mobile-icons">
+                <div className="grupo-iconos">
+                    <a className="logoFav" href="index.html#favoritos"></a>
+                    <a className="logoRegistro" href="index.html#registro"></a>
+                    <a className="logoCarrito" href="index.html#carrito"></a>
+                </div>
+                <button className="cerrar" id="closeMenu"></button> 
+            </div>
+            <hr className="hr-mobile" />
+            <ul className="links-menu">
+                <li> <a href="index.html">Inicio</a> </li>
+                <li> <a href="index.html#alta">Alta</a> </li>
+                <li> <a href="index.html#contacto">Contacto</a> </li>
+                <li> <a href="index.html#nosotros">Nosotros</a> </li>
+            </ul>
+            <div className="social-icons">
+                <div className="face"></div>
+                <div className="insta"></div>
+                <div className="tictoc"></div>
+            </div>
+
+        </div>
+
+    </div>
   
         <Outlet />
 
-      </div>
-    );
+      </body>
+    )
   }
 
   export default Layout
-  
